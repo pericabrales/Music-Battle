96bb71206a08905840ed92d0534984c69e257896
Peri Cabrales <43557105+pericabrales@users.noreply.github.com>
2020-10-27 22:15:08 -0700

    Added all files

---
+++ Music Battle/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1012357.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 36830520, 36830881, 36832224, 36832395, 36832549, 36834182, 36834203, 36838002, 36839582, 36839583, 36839584, 36839817 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/36839626/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/36839627/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/36839625/1/ammo.js', 'preload' : true},
+];
